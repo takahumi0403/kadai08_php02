0c6e863bd266104765d5ef3576e4d8c6368b4f9d
takahumi0403 <168268594+takahumi0403@users.noreply.github.com>
2024-06-30 19:30:05 +0900

    Add files via upload
    再度アップロード

---
+++ select.php
@@ -0,0 +1,71 @@
+<?php
+
+function h($str){
+    return htmlspecialchars($str,ENT_QUOTES,'UTF-8');
+
+}
+
+//1.  DB接続します
+try {
+  //ID:'root', Password:''
+  $pdo = new PDO('mysql:dbname=gs_php02_kadai;charset=utf8;host=localhost', 'root', '');
+} catch (PDOException $e) {
+  exit('DBConnectError:'.$e->getMessage());
+}
+
+//２．データ取得SQL作成
+$stmt = $pdo->prepare("SELECT*FROM gs_bm_table");
+$status = $stmt->execute();
+
+//３．データ表示
+$view="";
+if ($status==false) {
+    //execute（SQL実行時にエラーがある場合）
+  $error = $stmt->errorInfo();
+  exit("ErrorQuery:".$error[2]);
+
+}else{
+  //Selectデータの数だけ自動でループしてくれる
+  //FETCH_ASSOC=http://php.net/manual/ja/pdostatement.fetch.php
+  while( $result = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $view .= "<p>";
+    $view .= h($result['datetime']).' ' .h($result['bookname']) . ' '.h($result['bookURL'] ).' '. h($result['bookcomment']);
+    $view .= "</p>";
+  }
+
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>ブックマーク表示</title>
+<link rel="stylesheet" href="css/range.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body id="main">
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">データ登録</a>
+      </div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<div>
+    <div class="container jumbotron"><?= $view ?></div>
+</div>
+<!-- Main[End] -->
+
+</body>
+</html>
